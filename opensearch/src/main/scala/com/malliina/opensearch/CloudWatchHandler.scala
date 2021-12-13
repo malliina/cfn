bf1b3c60f82d999beb67a4ab77d9813490ee6289
Michael Skogberg <michael.skogberg@futurice.com>
2021-12-13 15:57:03 +0200

    Lambda

--- opensearch/src/main/scala/com/malliina/opensearch/CloudWatchHandler.scala
+++ opensearch/src/main/scala/com/malliina/opensearch/CloudWatchHandler.scala
@@ -8,6 +8,7 @@ import java.time.Instant
 class CloudWatchHandler extends RequestHandler[CloudWatchLogsEvent, String] {
   override def handleRequest(event: CloudWatchLogsEvent, context: Context): String = {
     val now = Instant.now()
+    val zipped = event.getAwsLogs.getData
     println(s"Hello, Lambda! The time is now $now.")
     s"Handled event '${event.getAwsLogs.getData}'."
   }
